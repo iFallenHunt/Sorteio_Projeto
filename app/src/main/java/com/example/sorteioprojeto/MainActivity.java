5d0c3c771200979a86908f5c2ea1e041dd2de584
iFallenHunt <lukas_santos.silva@hotmail.com>
2023-01-19 13:17:02 -0300

    First commit, random number app

---
+++ app/src/main/java/com/example/sorteioprojeto/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sorteioprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sortearNumero(View view) {
+
+        TextView texto = findViewById(R.id.textoResultado);
+        Random random = new Random();
+        int aleatorio = random.nextInt(10);
+        texto.setText("Número: " + aleatorio);
+
+
+    }
+
+}
